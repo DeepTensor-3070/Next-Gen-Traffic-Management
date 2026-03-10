ccdaaac84dbf5c08ca033f66ec5dfcbfaff7b0df
Deep Tensor <subhanshuverma24@gmail.com>
2026-03-10 14:04:17 +0530

    Code reset

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import cv2
 import serial
 import time
 
-arduino = serial.Serial('/dev/ttyACM0', 9600)
+arduino = serial.Serial('/dev/ttyACM0',9600)
 time.sleep(2)
 
 cap = cv2.VideoCapture(0)
 
 bg = cv2.createBackgroundSubtractorMOG2()
 
-count = 0
-line_y = 300
+cars = 0
+
+entry_line = 250
+exit_line = 350
 offset = 10
 
 while True:
@@ -19,7 +21,7 @@ while True:
     if not ret:
         break
 
-    frame = cv2.resize(frame, (640,480))
+    frame = cv2.resize(frame,(640,480))
 
     fgmask = bg.apply(frame)
 
@@ -27,7 +29,8 @@ while True:
 
     contours,_ = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.line(frame,(0,line_y),(640,line_y),(0,255,0),2)
+    cv2.line(frame,(0,entry_line),(640,entry_line),(0,255,0),2)
+    cv2.line(frame,(0,exit_line),(640,exit_line),(0,0,255),2)
 
     for cnt in contours:
 
@@ -36,25 +39,32 @@ while True:
         if area > 2500:
 
             x,y,w,h = cv2.boundingRect(cnt)
+
             cx = x + w//2
             cy = y + h//2
 
-            if line_y-offset < cy < line_y+offset:
+            # Entry detection
+            if entry_line-offset < cy < entry_line+offset:
+                cars += 1
+                print("Cars on road:",cars)
 
-                count += 1
-                print("Cars:",count)
-
-                arduino.write((str(count)+"\n").encode())
-                time.sleep(0.5)
+            # Exit detection
+            if exit_line-offset < cy < exit_line+offset:
+                cars -= 1
+                if cars < 0:
+                    cars = 0
+                print("Cars on road:",cars)
 
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-    cv2.putText(frame,"Cars: "+str(count),(20,50),
+    cv2.putText(frame,"Cars on Road: "+str(cars),(20,50),
                 cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
 
-    cv2.imshow("Vehicle Counter",frame)
+    arduino.write((str(cars)+"\n").encode())
 
-    if cv2.waitKey(1) == 27:
+    cv2.imshow("Traffic Monitor",frame)
+
+    if cv2.waitKey(1)==27:
         break
 
 cap.release()
